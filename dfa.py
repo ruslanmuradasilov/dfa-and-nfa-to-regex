987283d5a0f206f114e37576d39da8be8646437e
ruslanmuradasilov <muradasilov-r99@mail.ru>
2020-05-13 13:52:33 +0500

    modified input

--- dfa.py
+++ dfa.py
@@ -4,7 +4,8 @@ from graphviz import Digraph
 class DFA:
     def __init__(self, nstates, final_states, next_states_by_0, next_states_by_1):
         self.nstates = nstates
-        #self.init_state = 0
+        self.states = [i for i in range(nstates)]
+        self.init_state = 0
         self.final_states = final_states
         self.next_states_by_0 = next_states_by_0
         self.next_states_by_1 = next_states_by_1
@@ -19,12 +20,11 @@ class DFA:
 
 def main():
     nstates = input('Enter the number of states in your DFA: ')
-    final_states = input('Enter the final states: ')
-    final_states = final_states.split()
-    next_states_by_0 = input('Enter the next states by 0: ')
-    next_states_by_0 = next_states_by_0.split()
-    next_states_by_1 = input('Enter the next states by 1: ')
-    next_states_by_1 = next_states_by_1.split()
+    nstates = int(nstates)
+    final_states = list(map(int, input('Enter the final states: ').split()))
+    next_states_by_0 = list(map(int, input('Enter the next states by 0: ').split()))
+    next_states_by_1 = list(map(int, input('Enter the next states by 1: ').split()))
+
     dfa = DFA(nstates, final_states, next_states_by_0, next_states_by_1)
     dfa.draw_graph()
 
